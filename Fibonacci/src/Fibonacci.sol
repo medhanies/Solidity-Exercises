c4286e999ede9648fa6ff429b5edac2850a9ff78
medhanies <medhaniesolomon@yahoo.com>
2024-11-18 23:47:13 -0600

    Fibonacci function using for loop

--- Fibonacci/src/Fibonacci.sol
+++ Fibonacci/src/Fibonacci.sol
@@ -5,13 +5,22 @@ contract Fibonacci {
     /*
         This exercise assumes you understand what Fibonacci sequence is.
         1. Function `fibonacci` takes a uint256 as argument and returns nth fibonacci number.
-        
+
         Fibonacci sequence are 0,1,1,2,3,5,8,13,....
-        
+
         calling fibonacci(6) would return 8, because the 6th Fibonacci number is 8.
     */
 
-    function fibonacci(uint256 _position) public view returns (uint256) {
+    function fibonacci(uint256 _position) public pure returns (uint256) {
         // your code here
+        uint256 sum1 = 0;
+        uint256 sum2 = 1;
+        uint256 sum3;
+        for (uint256 i = 0; i < _position; i++) {
+            sum3 = sum1 + sum2;
+            sum2 = sum1;
+            sum1 = sum3;
+        }
+        return sum3;
     }
 }
